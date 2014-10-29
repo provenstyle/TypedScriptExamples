abb9553b63b80731b3e782421d4aa42977922c7a
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-10-29 17:37:36 -0500

    Typedscript object literal with a ko computed

---
+++ ObjectLiteralWithAKoComputed.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../Scripts/typings/knockout/knockout.d.ts"/>
+import ko = require('knockout');
+
+var vm = {
+    first: ko.observable("michael"),
+    last: ko.observable("dudley"),
+    fullName: ()=>{}
+}
+
+vm.fullName = ko.computed(() => {
+    return vm.first() + " " + vm.last();
+});
+
+export = vm;
