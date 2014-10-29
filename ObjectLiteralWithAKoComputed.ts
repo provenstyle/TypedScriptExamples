edb9cdae44c10c18afe634efdc7cf56b5247d9f6
Michael Dudley <Michael.Dudley@ImprovingEnterprises.com>
2014-10-29 17:42:15 -0500

    commenting the code

--- ObjectLiteralWithAKoComputed.ts
+++ ObjectLiteralWithAKoComputed.ts
@@ -1,12 +1,13 @@
-﻿/// <reference path="../Scripts/typings/knockout/knockout.d.ts"/>
+/// <reference path="../Scripts/typings/knockout/knockout.d.ts"/>
 import ko = require('knockout');
 
 var vm = {
     first: ko.observable("michael"),
     last: ko.observable("dudley"),
-    fullName: ()=>{}
+    fullName: ()=>{} //You have to define an empty function on the object
 }
 
+//And then create the computed function 
 vm.fullName = ko.computed(() => {
     return vm.first() + " " + vm.last();
 });
